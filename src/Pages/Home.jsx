cc3e9bd12609f04a9fd36c484a2e736e32767e32
bojjapusrinivas <77658911+pixel4inch@users.noreply.github.com>
2023-11-09 13:20:50 +0530

    home page update

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
 
+import { Row, Container,Col } from 'reactstrap'
+
 import Helmet from '../Components/Helmet/Helmet'
 
 function Home() {
+  const year = new Date().getFullYear();
   return  <Helmet title={"HOME"}>
+<section className='hero_section p-2'>
+    <Container>
+       <Row>
+            <Col lg="6" md="6">
+                <div className='hero_content'>
+                    <p className='hero_subtitle'>Trending product {year}</p>
+                    <h2 className="hero_Title"> Make Your Interior More Minimalistic  Modern</h2>
+            
+                </div>
+            </Col>
+        <Col lg="6">
+        
+        </Col>
+       </Row>
+    </Container>
 
-
+</section>
 
 
 
