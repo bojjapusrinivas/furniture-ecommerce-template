706cb1bd984146aac852d50c2d31ab4b8b6329c0
bojjapusrinivas <77658911+pixel4inch@users.noreply.github.com>
2023-11-28 16:59:23 +0530

    common section updated

---
+++ src/Components/UI/CommonSection/Commonsection.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import"./commonsection.css"
+
+function Commonsection({title}) {
+  return (
+    <>
+    <section className="commonsection">
+        <h2>{title}</h2>
+    </section>
+    
+    </>
+  )
+}
+
+export default Commonsection
--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import Helmet from '../Components/Helmet/Helmet'
+import Commonsection from '../Components/UI/CommonSection/Commonsection'
 
 function Shop() {
   return  <Helmet title={"SHOP"}>
 
-SHOP
-
-
+    <Commonsection title="PRODUCTS"/>
 
   </Helmet>
 }
