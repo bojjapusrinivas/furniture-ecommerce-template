02c793479ebe683f5fd431dba452a26af2da417e
bojjapusrinivas <77658911+pixel4inch@users.noreply.github.com>
2023-12-04 13:13:31 +0530

    shop page updated

--- src/Pages/ProductDetails.jsx
+++ src/Pages/ProductDetails.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import Helmet from '../Components/Helmet/Helmet'
+import Commonsection from '../Components/UI/CommonSection/Commonsection'
+import products from '../assets/data/products'
+import { useParams } from 'react-router-dom'
+import { Container,Row,Col } from 'reactstrap'
+
+
 
 function ProductDetails() {
   return  <Helmet title={"PRODUCT DETAILS"}>
 
-PRODUCT DETAILS
+  const {id}=useParams();
+  const product = products.find(item = item.id === id)
+
+  <Commonsection/>
+  <Container>
+    <Row>
+      <Col lg="6" sm="12">
+
+      </Col>
+
+      <Col lg="6" sm="12">
+        
+      </Col>
+    </Row>
+  </Container>
+  
 
 
 
